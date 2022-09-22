cb593f5c59d7f96e75625b80e3d2b4487997d7c5
Luiz Franzone <luizgustavocf@gmail.com>
2022-09-22 16:07:20 -0300

    Aula 03 - Variáveis e tipos de dados

--- main.go
+++ main.go
@@ -2,6 +2,28 @@ package main
 
 import "fmt"
 
+var (
+ nome string
+ n1 int
+ n2 int32
+)
+
 func main(){
- fmt.Println("Hello World!")
+ var mensagem string = "Aula 03 - Go 101"
+ fmt.Println(mensagem)
+
+ var b, f, s = true, 2.3, "Ölá"
+ fmt.Println(b, f, s)
+
+ var total int
+ total++
+ fmt.Println("total:", total)
+
+ nome = "Luiz Gustavo"
+ fmt.Println("Hello", nome, "!")
+
+ var x, y = 10, 20
+ fmt.Println(x, y)
+ x, y = y, x
+ fmt.Println(x, y)
 }
